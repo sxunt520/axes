a0fafb965d9d0873fd6e4d607e6e2cdebdd99fbb
始渲 <if43vip@gmail.com>
2020-09-26 16:42:49 +0800

    官网第二版

--- frontend/themes/basic/layouts/main.php
+++ frontend/themes/basic/layouts/main.php
@@ -7,27 +7,31 @@ use yii\helpers\Html;
 use frontend\assets\AppAsset;
 AppAsset::register($this);
 ?>
-
 <?php $this->beginPage() ?>
     <!DOCTYPE html>
-    <html lang="zh-cn">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title><?= Html::encode($this->title) ?></title>
-        <meta name="keywords" content="">
-        <meta name="description" content="">
-        <link rel="stylesheet" href="/css/reset.css">
-        <link rel="stylesheet" href="/css/index.css">
-        <?php $this->head() ?>
-    </head>
-        <body class="home">
+    <html style="overflow: hidden; height: 100%;" lang="zh-CN">
+        <head>
+            <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+            <meta charset="utf-8">
+            <title><?= Html::encode($this->title) ?></title>
+            <meta name="keywords" content="">
+            <meta name="description" content="">
+            <link rel="stylesheet" href="/css/fullPage.css">
+            <link rel="stylesheet" href="/css/my_index.css">
+            <style>
+                .section { text-align: center; font: 50px "Microsoft Yahei"; color: #fff;}
+            </style>
+            <script src="/js/jquery_002.js"></script>
+            <script src="/js/fullPage.js"></script>
+            <?php $this->head() ?>
+        </head>
+        <body>
             <?php $this->beginBody() ?>
 
-                <?= $content ?>
+             <?= $content ?>
 
             <?php $this->endBody() ?>
         </body>
     </html>
 <?php $this->endPage() ?>
+
--- frontend/themes/basic/site/index.php
+++ frontend/themes/basic/site/index.php
@@ -4,182 +4,224 @@
 
 $this->title = '旅人计划';
 ?>
-<header class="header" id="header">
-    <div class="header-content clearfix">
-        <h1 class="header-logo">
-            <a href="/" class="header-logo-link">旅人计划</a>
-        </h1>
-        <ul class="header-nav">
-            <!--<li class="active"><a href="https://www.soulapp.cn/">首页</a></li>
-            <li class="header-nav-menu" id="menu">
-              <a>加入我们</a>
-              <span class="icon-arrow-bottom"></span>
-              <div class="menu">
-                <div class="menu-triangle"></div>
-                <ul class="menu-items">
-                  <li class="menu-item"><a href="https://app.mokahr.com/apply/soul/7146#/jobs" target="_blank">社会招聘</a></li>
-                  <li class="menu-item"><a href="https://app.mokahr.com/campus_apply/soul/7147#/jobs?_k=tw3fhv" target="_blank">校园招聘</a></li>
-                  <li class="menu-item"><a href="https://www.soulapp.cn/outside.html">外部推荐</a></li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <a href="https://www.soulapp.cn/about.html">关于我们</a>
-            </li>-->
-            <li class="header-nav-audio muted" id="audioControl"></li>
-            <!--<li class="header-nav-IM"><a href="#" target="_blank"></a></li>-->
-        </ul>
+<header class="header">
+    <div class="header-content">
+        <h1 class="header-logo"><a href="/" class="header-logo-link">旅人计划</a></h1>
     </div>
 </header>
+<div id="dowebok">
 
-<section class="section section-main section-active" style=" background: #fff url(/images/home_bg.jpg) no-repeat center center/cover;">
-    <div class="video-container">
-
-<!--        <div class="img_box" style=" background:url(/images/home_bg.jpg) repeat center top;"></div>-->
-<!--         <video muted="muted" loop="loop" autoplay="autoplay" src="http://192.168.0.149:188/uploads/demo_mp4/home_head.mp4" class="video" id="video-home" data-object-fit="">抱歉，您的浏览器不支持内嵌视频</video>-->
-
-    </div>
-    <div class="main-content">
-        <p><img src="/images/story_traveling.png" alt="跟随灵魂找到你"></p>
-        <!--<p>温暖的灵魂，在此相遇</p>-->
-
-        <div class="main-down clearfix">
-            <a class="main-down-ios" href="#" target="_blank">
-                <img src="/images/img-qrcode-ios.png">
-            </a>
-            <a class="main-down-and" href="#" target="_blank">
-                <img src="/images/img-qrcode-and.png">
-            </a>
+    <div class="section" style="background:url(/images/home_bg.jpg) no-repeat center center/cover">
+        <div class="box1">
+            <div class="header_title"></div>
+            <div class="down_c">
+                <div class="android_down"><span></span><a href="javascript:void(0);"></a></div>
+                <div class="apple_down"><span></span><a href="javascript:void(0);"></a></div>
+            </div>
         </div>
     </div>
-</section>
 
-<div class="list-container">
-
-    <section class="section section-soul">
-        <div class="section-content clearfix">
-            <div class="section-left">
-                <div style="margin-left:64px; margin-top:10px; width:269px; height:543px; overflow:hidden;">
-                    <img src="/images/phone_box/box_img_1.png" />
-                    <!--<video id="my-video1" muted="muted" loop="loop" src="/images/demo_video.mp4" width="269" height="543" class="my-video1" data-object-fit="">抱歉，您的浏览器不支持内嵌视频</video>-->
+    <div class="section">
+        <div class="myBox">
+            <div class="myBox_c">
+                <div class="myBox_l">
+                    <div class="phone_xxx phone_xxx_1"></div>
+                    <div class="phone_img phone_img_1"></div>
+                    <div class="can_u_speak_chinese"><img src="/images/can_u_speak_chinese.png" width="90%"></div>
+                    <div class="can_u_speak_japanese"><img src="/images/can_u_speak_japanese.png" width="90%"></div>
                 </div>
-
-                <div class="section-icon soul-phone"></div>
-                <div class="section-icon soul-5"></div>
-                <div class="section-icon soul-6"></div>
-            </div>
-            <div class="section-right">
-                <!--<h2 class="right-title">灵魂匹配</h2>
-                <p class="right-sub">属于你的灵魂伴侣</p>
-                <p class="right-desc">
-                  分享生活与热爱。AI 智能精准匹配，随时随地<br>与志趣相投的 Souler 开启会话，和尬聊说拜拜。
-                </p>
-                <p class="right-tip">来 Soul 吧，分享生活每一个闪光时刻。</p>-->
-                <h2 class="right-title" style="padding-top:100px;"><img src="/images/cont_title_1.jpg" /></h2>
+                <div class="myBox_r"><img src="/images/pick_1.png" width="235" height="114"></div>
             </div>
         </div>
-    </section>
+    </div>
 
-
-    <section class="section section-soul">
-        <div class="section-content clearfix">
-            <div class="section-left">
-                <div style="margin-left:145px; margin-top:10px; width:269px; height:543px; overflow:hidden;">
-                    <img src="/images/phone_box/box_img_2.png" />
+    <div class="section">
+        <div class="myBox">
+            <div class="myBox_c">
+                <div class="myBox_l">
+                    <div class="phone_xxx phone_xxx_2"></div>
+                    <div class="phone_img phone_img_2"></div>
+                    <div class="like_xing"></div>
+                    <a class="hover_like" href="javascript:void(0);"><span></span></a>
                 </div>
-                <div class="section-icon soul-phone2"></div>
-                <div class="section-icon hover_like"></div>
-            </div>
-            <div class="section-right">
-                <h2 class="right-title" style="padding-top:100px;"><img src="/images/cont_title_2.jpg" /></h2>
+                <div class="myBox_r"><img src="/images/pick_2.png" width="410" height="113"></div>
             </div>
         </div>
-    </section>
+    </div>
 
-    <section class="section section-soul">
-        <div class="section-content clearfix">
-            <div class="section-left">
-                <div style="margin-left:145px; margin-top:10px; width:269px; height:543px; overflow:hidden;">
-                    <img src="/images/phone_box/box_img_3.png" />
+    <div class="section">
+        <div class="myBox">
+            <div class="myBox_c">
+                <div class="myBox_l">
+                    <div class="phone_xxx phone_xxx_3"></div>
+                    <div class="phone_img phone_img_3"></div>
+
+                    <div class="section-freeicon free_ico"><img src="/images/free_ico.png" width="90%"></div>
+                    <div class="section-freeicon free_text1"><img src="/images/free_text1.png" width="90%"></div>
+                    <div class="section-freeicon free_text2"><img src="/images/free_text2.png" width="90%"></div>
+                    <div class="section-freeicon free_text3"><img src="/images/free_text3.png" width="90%"></div>
+                    <div class="section-freeicon free_text4"><img src="/images/free_text4.png" width="90%"></div>
+
                 </div>
-                <div class="section-icon soul-phone3"></div>
-                <div class="section-icon free_ico"></div>
-                <div class="section-icon free_text1"></div>
-                <div class="section-icon free_text2"></div>
-                <div class="section-icon free_text3"></div>
-                <div class="section-icon free_text4"></div>
-            </div>
-            <div class="section-right">
-                <h2 class="right-title" style="padding-top:100px;"><img src="/images/cont_title_3.jpg" /></h2>
+                <div class="myBox_r"><img src="/images/pick_3.png" width="362" height="114"></div>
             </div>
         </div>
-    </section>
+    </div>
 
-    <!--<section class="section section-soul">
-      <div class="section-content clearfix">
-        <div class="section-left">
-          <div style="margin-left:225px; margin-top:10px; width:269px; height:543px; overflow:hidden;">
-         <img src="/images/phone_box/box_img_4.png" />
-          </div>
-          <div class="section-icon soul-phone4"></div>
-          <div class="section-icon hover_like"></div>
-        </div>
-        <div class="section-right">
-          <h2 class="right-title" style="padding-top:100px;"><img src="/images/cont_title_4.jpg" /></h2>
-        </div>
-      </div>
-    </section>-->
+    <div class="section">
+        <div class="myBox">
+            <div class="myBox_c">
+                <div class="myBox_l">
+                    <div class="phone_xxx phone_xxx_4"></div>
+                    <div class="phone_img phone_img_4"></div>
 
-    <section class="section section-square">
-        <div class="section-content clearfix">
-            <div class="section-left">
-                <div class="section-phone-content"></div>
-                <div class="section-phone4"></div>
-                <!--<div class="section-icon square-1"></div>-->
-            </div>
-            <div class="section-right">
-                <h2 class="right-title" style="padding-top:100px;"><img src="/images/cont_title_4.jpg" /></h2>
+                </div>
+                <div class="myBox_r"><img src="/images/pick_4.png" width="410" height="114"></div>
             </div>
         </div>
-    </section>
-
-    <div class="list-bg">
-        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-            <defs>
-                <path id="gentle-wave" class="st0" d="M-160,35.6 c 30,0,58-6.6,88-6.6 s58,6.6,88,6.6s58-6.6,88-6.6s58,6.6,88,6.6V59h-352V35.6z"></path>
-                <linearGradient id="primary" x1="0%" y1="0%" x2="0%" y2="50%">
-                    <stop offset="0%" style="stop-color:#f38b91; stop-opacity:1"></stop>
-                    <stop offset="100%" style="stop-color:#b30511; stop-opacity:1"></stop>
-                </linearGradient>
-            </defs>
-            <g class="parallax">
-                <use style="fill:url(#primary)" xlink:href="#gentle-wave" x="48" y="0"></use>
-                <use style="fill:url(#primary)" xlink:href="#gentle-wave" x="48" y="0"></use>
-            </g>
-        </svg>
     </div>
 
 </div>
 
-<!--index.js 里this.length设置鼠标滚动的次数-->
-<div class="aside">
-    <ul class="aside-list" id="aside">
-        <li data-index="0" class="active"></li>
-        <li data-index="1"></li>
-        <li data-index="2"></li>
-        <li data-index="3"></li>
-        <li data-index="4"></li>
-    </ul>
-</div>
+<div id="down_float_r" style="display: none;"><a href="javascript:void(0);"></a><div class="float_qrcode"><img src="/images/down_float.png" width="200" height="420"></div></div>
 
+<footer class="footer" id="footer" style=" display:none;"></footer>
 
-<div class="qrcode" id="qrcode">
-    <div class="qrcode-mask" id="qrcode-mask"></div>
-    <div class="qrcode-content">
-        <img src="/images/img-qrcode-ios.png">
-        <p> 扫一扫畅玩超多精彩活动</p>
-    </div>
-</div>
+<script>
+    $(function(){
+        $('#dowebok').fullpage({
+            sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff'],
+            'navigation': true,
+            loopBottom: true,  //滚动到最底部后是否滚回顶部
+            'navigationColor':'#E9292C',
+            afterLoad: function(anchorLink, index){
 
-<!--<script src="js/hm.js"></script>-->
-<script src="/js/index.js"></script>
+                if(index == 1){
+                    $('#footer').hide();
+                    $('#down_float_r').hide();
+                }
+                if(index == 2){
+                    $('#footer').fadeIn("slow");
+                    $('#down_float_r').show();
+
+                    $(".can_u_speak_chinese img").animate({
+                        width: "100%",
+                        height: "100%",
+                    }, 500 ).animate({
+                        width: "90%",
+                        height: "90%",
+                    }, 500 );
+
+                    setTimeout(function(){
+                        $(".can_u_speak_japanese img").animate({
+                            width: "100%",
+                            height: "100%",
+                        }, 500 ).animate({
+                            width: "90%",
+                            height: "90%",
+                        }, 500 );
+                    }, 1000);
+
+                }
+                if(index == 3){
+                    $('#footer').show();
+                    $('#down_float_r').show();
+
+                }
+                if(index == 4){
+                    $('#footer').show();
+                    $('#down_float_r').show();
+
+                    $(".free_ico img").animate({
+                        width: "100%",
+                        height: "100%",
+                    }, 500 ).animate({
+                        width: "90%",
+                        height: "90%",
+                    }, 500 );
+
+                    setTimeout(function(){
+                        $(".free_text1 img").animate({
+                            width: "100%",
+                            height: "100%",
+                        }, 500 ).animate({
+                            width: "90%",
+                            height: "90%",
+                        }, 500 );
+                    }, 1000);
+
+                    setTimeout(function(){
+                        $(".free_text2 img").animate({
+                            width: "100%",
+                            height: "100%",
+                        }, 500 ).animate({
+                            width: "90%",
+                            height: "90%",
+                        }, 500 );
+                    }, 1500);
+
+                    setTimeout(function(){
+                        $(".free_text3 img").animate({
+                            width: "100%",
+                            height: "100%",
+                        }, 500 ).animate({
+                            width: "90%",
+                            height: "90%",
+                        }, 500 );
+                    }, 2000);
+
+                    setTimeout(function(){
+                        $(".free_text4 img").animate({
+                            width: "100%",
+                            height: "100%",
+                        }, 500 ).animate({
+                            width: "90%",
+                            height: "90%",
+                        }, 500 );
+                    }, 2500);
+
+                }
+
+            },
+            onLeave: function(index, direction){
+                if(index == 1){
+                    $('#footer').hide();
+                    $('#down_float_r').hide();
+                }
+                if(direction == 1){
+                    $('#footer').hide();
+                    $('#down_float_r').hide();
+                }
+            }
+        });
+        $.fn.fullpage.setScrollingSpeed(500);//滚动速度
+        setInterval(function(){
+            $.fn.fullpage.moveSectionDown();
+        }, 10000);
+
+        $(".android_down").mouseover(function(){
+            $(".android_down span").show();
+        }).mouseout(function(){
+            $(".android_down span").hide();
+        });
+
+        $(".apple_down").mouseover(function(){
+            $(".apple_down span").show();
+        }).mouseout(function(){
+            $(".apple_down span").hide();
+        });
+
+        $("#down_float_r a").mouseover(function(){
+            $("#down_float_r .float_qrcode").show();
+        }).mouseout(function(){
+            $("#down_float_r .float_qrcode").hide();
+        });
+
+        $(".myBox_l a.hover_like").mouseover(function(){
+            $(".myBox_l a.hover_like span").show();
+        }).mouseout(function(){
+            $(".myBox_l a.hover_like span").hide();
+        });
+
+    });
+</script>
