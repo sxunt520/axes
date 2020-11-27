76e6cd49436e8027bd6234f83aa23316d64a6f4b
sxunt <if43vip@gmail.com>
2020-11-27 15:32:42 +0800

    视频无法传咯，那个鬼给我注释的上传视频的那行代码，好日怪

--- backend/views/story-recommend/_form.php
+++ backend/views/story-recommend/_form.php
@@ -39,37 +39,37 @@ use kartik\file\FileInput;
         $video_url=null;
     }
     echo $form->field($model,'_video_url')->label('推荐视频')->widget(FileInput::classname(), [
-        //                'options' => ['accept' => 'video/*'],
-        //                'pluginOptions' => [
-        //                    // 需要预览的文件格式
-        //                    'previewFileType' => 'video',
-        //                    // 预览的文件
-        //                    'initialPreview' => [$video_url],
-        //                    // 异步上传的接口地址设置
-        //                    'uploadUrl' => \yii\helpers\Url::toRoute(['async-upcos']),
-        //                    //'uploadUrl' => \yii\helpers\Url::toRoute(['async-video']),
-        //                    'uploadAsync' => true,
-        //                    // 最少上传的文件个数限制
-        //                    'minFileCount' => 1,
-        //                    // 最多上传的文件个数限制
-        //                    'maxFileCount' => 1,
-        //                    //'showPreview'=>false,//是否显示整个文件区，自然就无法拖曳文件进行上传了
-        //                ],
-        //                // 一些事件行为
-        //                    'pluginEvents' => [
-        //                        // 上传成功后的回调方法，需要的可查看data后再做具体操作，一般不需要设置
-        //                        "fileuploaded" => "function (event, data, id, index) {
-        //                                    console.log(data);
-        //                                    //console.log(data.response.initialPreview[0]);
-        //                                    //console.log($('input[StoryRecommend][video_url]'));
-        //                                    $(\"input[name='StoryRecommend[video_url]']\").val(data.response.video_url);
-        //                                    if(data.response.video_cover_flag==true){//如果有视频封面图
-        //                                        $(\"#video_cover_img\").attr('src',data.response.video_cover_url);
-        //                                        $(\"#xxx-upimg\").val(data.response.video_cover_url);
-        //                                    }
-        //                                }",
-        //                    ],
-        //                    //'pluginLoading'=>false,
+                        'options' => ['accept' => 'video/*'],
+                        'pluginOptions' => [
+                            // 需要预览的文件格式
+                            'previewFileType' => 'video',
+                            // 预览的文件
+                            'initialPreview' => [$video_url],
+                            // 异步上传的接口地址设置
+                            'uploadUrl' => \yii\helpers\Url::toRoute(['async-upcos']),
+                            //'uploadUrl' => \yii\helpers\Url::toRoute(['async-video']),
+                            'uploadAsync' => true,
+                            // 最少上传的文件个数限制
+                            'minFileCount' => 1,
+                            // 最多上传的文件个数限制
+                            'maxFileCount' => 1,
+                            //'showPreview'=>false,//是否显示整个文件区，自然就无法拖曳文件进行上传了
+                        ],
+                        // 一些事件行为
+                            'pluginEvents' => [
+                                // 上传成功后的回调方法，需要的可查看data后再做具体操作，一般不需要设置
+                                "fileuploaded" => "function (event, data, id, index) {
+                                            console.log(data);
+                                            //console.log(data.response.initialPreview[0]);
+                                            //console.log($('input[StoryRecommend][video_url]'));
+                                            $(\"input[name='StoryRecommend[video_url]']\").val(data.response.video_url);
+                                            if(data.response.video_cover_flag==true){//如果有视频封面图
+                                                $(\"#video_cover_img\").attr('src',data.response.video_cover_url);
+                                                $(\"#xxx-upimg\").val(data.response.video_cover_url);
+                                            }
+                                        }",
+                            ],
+                            //'pluginLoading'=>false,
     ]);?>
 
     <div class="form-group field-storyrecommend-cover_url required">
