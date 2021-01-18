9130de057eefb5c287ca4a4ce235303ae064ea8b
axes_admin <wenjianbo@233i.com>
2021-01-18 09:48:06 +0800

    update

--- frontend/themes/mobile/site/index.php
+++ frontend/themes/mobile/site/index.php
@@ -35,7 +35,7 @@ $this->title = '超S级互动故事阅读APP';
     </div>
 </div>
 <div class="icp">
-    <p><a target="_blank" href="http://www.beian.miit.gov.cn/"> © 2020 www.233i.com All rights reserved 蜀ICP备18015972号 川网文（2018）9245-339号</a></p>
+    <p><a target="_blank" href="https://beian.miit.gov.cn/"> © 2020 www.233i.com All rights reserved 蜀ICP备18015972号-1 川网文（2018）9245-339号</a></p>
 </div>
 <script src="/m_static/js/jquery.js"></script>
 <script src="/m_static/js/index.js"></script>
